3cff62abb4ef36b2bab8baf513136923dc7cdc0b
Emanuel Rodrigues Galvão <emrogalvao@gmail.com>
2024-03-03 19:04:27 -0300

    chore: add koin to dependency injection

--- app/src/main/java/com/emanuelgalvao/qualocarro/repository/ApiResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.emanuelgalvao.qualocarro.repository
-
-import com.emanuelgalvao.qualocarro.model.Vehicle
-
-sealed interface ApiResponse {
-    class Error(val message: String): ApiResponse
-    class Success(val data: Vehicle): ApiResponse
-}
--- app/src/main/java/com/emanuelgalvao/qualocarro/repository/VehicleRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.emanuelgalvao.qualocarro.repository
-
-interface VehicleRepository {
-
-    suspend fun findVehicle(plate: String): ApiResponse
-}
